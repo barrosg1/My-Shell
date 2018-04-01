41c87bcd5debcd1ae7861b8f62bbc7ae0648b128
Gabriel <mcgabe11@gmail.com>
2018-03-31 21:15:54 -0400

    first revision

--- main.c
+++ main.c
@@ -13,102 +13,26 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <fcntl.h>
 
 #define ARG_BUF_SIZE 1024
 #define TRUE 1
 #define FALSE 0
 
 
+void mini_shell_loop(int, FILE*);
+
+
 int main(int argc, char *argv[])
 {
 
+	int batchMode = TRUE;
 
 	if(argc == 1)
 	{ // interactive mode
 
-		do
-		{ /* for each prompt */
-
-			pid_t pid;
-			int status;
-			int is_background = FALSE, barrier = FALSE;
-			int buffer_size = ARG_BUF_SIZE, i=0;
-			char* line = calloc(sizeof(char), buffer_size);
-			char** args = calloc(sizeof(char*), buffer_size);
-			char* token;
-
-			printf("miniShell> ");
-			fgets(line, buffer_size, stdin);
-
-			if(!args)
-			{
-				fprintf(stderr, "Cannot allocate memory.\n");
-				exit(0);
-			}
-
-			token = strtok(line, " \t\r\n\a");
-			while(token != NULL)
-			{
-				args[i] = token;
-				i++;
-
-				token = strtok(NULL, " \t\r\n\a");
-			}
-
-			if(strcmp(args[0], "quit") == 0) /* if user types quit */
-				exit(0);
-
-			if(args[i-1][strlen(args[i-1])-1] == '&')
-			{
-				args[i-1][strlen(args[i-1])-1] = '\0';
-				is_background = TRUE;
-			}
-			else if(strcmp(args[0], "barrier") == 0 || strcmp(args[0], "barrier&") == 0)
-			{
-
-				args[0] = NULL;  /* clearing 'barrier' so execvp doesn't try to execute it */
-				barrier = TRUE;
-			}
-
-			pid = fork(); // spawning a process
-
-			if(pid < 0)
-			{
-				perror("Fork Error");
-				exit(0);
-			}
-			else if(pid == 0)
-			{ /* CHILD process */
-
-
-				if(execvp(args[0], args) == -1)
-				{
-					perror("Execute Command");
-					exit(0);
-				}
-
-			}
-			else
-			{ /* PARENT process */
-
-				if(!is_background)
-				{ /* if process is not in background then wait for current process to finish */
-
-					waitpid(pid, &status, 0);
-				}
-
-				if(barrier)
-				{ /* waiting for all processes to finish before displaying next prompt */
-
-					while((pid = waitpid(-1, &status, 0)) != -1);
-				}
-
-			}
-
-			free(line);
-			free(args);
-
-		}while(1);
+		batchMode = FALSE;
+		mini_shell_loop(batchMode, NULL);
 
 	}
 	else if(argc > 2)
@@ -117,12 +41,130 @@ int main(int argc, char *argv[])
 		exit(0);
 	}
 	else
-	{
-		puts("Batch mode");
-	}
+	{ // batch mode
 
+		FILE* fp;
+		char* filename = argv[1];
+
+		fp = fopen(filename, "r");
+		if(fp == NULL)
+		{
+			perror("Error opening the file");
+			exit(0);
+		}
+
+		mini_shell_loop(batchMode, fp);
+
+	}
 
 	return EXIT_SUCCESS;
 }
+
+
+void mini_shell_loop(int batchMode, FILE* file)
+{
+	do
+	{ /* for each prompt */
+
+		pid_t pid;
+		int status;
+		int doInBackground = FALSE, barrier = FALSE;
+		int buffer_size = ARG_BUF_SIZE, i=0;
+		char* line = calloc(sizeof(char), buffer_size);
+		char** args = calloc(sizeof(char*), buffer_size);
+		char* token;
+
+		while((pid = waitpid(-1, &status, WNOHANG)) > 0); /* background processes termination */
+
+		if(!batchMode)
+		{
+			printf("miniShell> ");
+			fgets(line, buffer_size, stdin); /* read command in interactive mode */
+
+		}
+		else
+		{
+			fgets(line, buffer_size, file); /* read file in batch mode */
+
+			if(feof(file)) exit(0); /* if end of file is reached */
+		}
+
+		if(!args)
+		{
+			fprintf(stderr, "Cannot allocate memory.\n");
+			exit(0);
+		}
+
+		token = strtok(line, " \t\r\n\a");
+		while(token != NULL)
+		{
+			args[i] = token;
+			i++;
+
+			token = strtok(NULL, " \t\r\n\a");
+		}
+
+		if(args[0] == '\0') continue;
+		if(strcmp(args[0], "quit") == 0) exit(0); /* if user types quit */
+
+		if(strcmp(args[0], "barrier") == 0 || strcmp(args[0], "barrier&") == 0)
+		{
+			args[0] = NULL;  /* clearing 'barrier' so execvp doesn't try to execute it */
+			barrier = TRUE;
+		}
+		else if(args[i-1][strlen(args[i-1])-1] == '&')
+		{
+			args[i-1][strlen(args[i-1])-1] = '\0';
+			doInBackground = TRUE;
+		}
+
+
+		/* ----------------------- Handling processes ----------------------- */
+
+		pid = fork(); // spawning a process
+
+		if(pid < 0)
+		{
+			perror("Fork Error");
+			exit(0);
+		}
+		else if(pid == 0)
+		{ /* CHILD process */
+
+			if(execvp(args[0], args) == -1)
+			{
+				fprintf(stderr, "%s: command not found\n", args[0]);
+				break;
+			}
+
+		}
+		else
+		{ /* PARENT process */
+
+			if(!doInBackground)
+			{ /* if process is not in background then wait for current process to finish */
+				//printf("Spawned foreground process pid: %d\n", pid);
+
+				waitpid(pid, &status, 0);
+
+				//printf("foreground process pid %d terminated\n", pid);
+			}
+
+
+			if(barrier)
+			{ /* waiting for all processes to finish before displaying next prompt */
+
+				while((pid = waitpid(-1, &status, 0)) != -1);
+			}
+
+		}
+
+		free(line);
+		free(args);
+
+	}while(1);
+}
+
+
 
 
